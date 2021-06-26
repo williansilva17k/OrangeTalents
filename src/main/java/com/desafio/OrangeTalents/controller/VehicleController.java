a33d8f08e8ae2f79bdbba450a081825bf65e2c28
Willian Evangelista Silva <willianevangelistasilva17@gmail.com>
2021-06-26 18:54:37 -0300

    fix: removing unused comments

--- src/main/java/com/desafio/OrangeTalents/controller/VehicleController.java
+++ src/main/java/com/desafio/OrangeTalents/controller/VehicleController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public interface VehicleController {
-    //http://localhost:8080/veiculo/carros/marcas/59/modelos/5940/anos/2014-3/user/13086205601
-    //http://localhost:8080/veiculo/carros/marcas/10/modelos/259/anos/1994-1/user/13086205601
+
     @PostMapping(value = "/{type}/marcas/{marca_id}/modelos/{modelo_id}/anos/{ano}/user/{cpf}")
     ResponseEntity saveVeiculo(@PathVariable String cpf, @PathVariable String type,
                                @PathVariable Integer marca_id, @PathVariable Integer modelo_id,
